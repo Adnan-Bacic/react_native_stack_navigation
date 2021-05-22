58345e8adf0a3f53cb07bdd171fd0baef996baba
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-05-22 19:24:41 +0200

    linter setup and cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:jest/recommended',
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+    'react-native',
+    'react-hooks',
+    'import',
+    'jsx-a11y',
+    'jest',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
